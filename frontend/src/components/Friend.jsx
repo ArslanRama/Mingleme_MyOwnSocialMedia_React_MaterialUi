fceed898b748211075aa09d10efabf4055d5e73c
Aslan <arslanrama84@gmail.com>
2023-04-14 21:30:55 +0200

    adjust friends and userimage

--- frontend/src/components/Friend.jsx
+++ frontend/src/components/Friend.jsx
@@ -9,17 +9,14 @@ import UserImage from "./UserImage";
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { _id } = useSelector((state) => state.user);
-  const token = useSelector((state) => state.token);
-  const friends = useSelector((state) => state.user.friends);
+  const { _id, token, user: { friends } } = useSelector((state) => state);
 
-  const { palette } = useTheme();
-  const primaryLight = palette.primary.light;
-  const primaryDark = palette.primary.dark;
-  const main = palette.neutral.main;
-  const medium = palette.neutral.medium;
+  const { palette: { primary, neutral } } = useTheme();
+  const { light: primaryLight, dark: primaryDark } = primary;
+  const main = neutral.main;
+  const medium = neutral.medium;
 
-  const isFriend = friends.find((friend) => friend._id === friendId);
+  const isFriend = friends.some((friend) => friend._id === friendId);
 
   const patchFriend = async () => {
     const response = await fetch(
@@ -36,23 +33,23 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     dispatch(setFriends({ friends: data }));
   };
 
+  const navigateToProfile = () => {
+    navigate(`/profile/${friendId}`);
+    navigate(0);
+  };
+
   return (
     <FlexBetween>
       <FlexBetween gap="1rem">
         <UserImage image={userPicturePath} size="55px" />
-        <Box
-          onClick={() => {
-            navigate(`/profile/${friendId}`);
-            navigate(0);
-          }}
-        >
+        <Box onClick={navigateToProfile}>
           <Typography
             color={main}
             variant="h5"
             fontWeight="500"
             sx={{
               "&:hover": {
-                color: palette.primary.light,
+                color: primaryLight,
                 cursor: "pointer",
               },
             }}
@@ -65,7 +62,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
         </Box>
       </FlexBetween>
       <IconButton
-        onClick={() => patchFriend()}
+        onClick={patchFriend}
         sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
       >
         {isFriend ? (
